c4e0f2a3b96edca38a5623ed3c0982b08a6aad9f
Paulo Patto <paulopatto@paulopatto.com>
2017-10-06 19:16:49 -0300

    Add powerline instalation.

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,6 +37,7 @@ sudo apt-get install -y python3-dev python3-pip
 echo "---> Install Pyhton PIP packages"
 pip3 install --upgrade pip
 pip3 install --user ipython 
+pip3 install --user git+git://github.com/Lokaltog/powerline
 pip3 install --user powerline-status
 pip3 install --user jupyter
 echo "\t Done!\n"

74a338527a745de77607ab98866fe647edc65400
Paulo Patto <paulopatto@paulopatto.com>
2018-07-16 11:42:55 +0000

    Surround Linux/Debian|Ubuntu configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ ln -s $PWD/xresources.d/xresources $HOME/.Xresources
 echo "\t---> Create a symlink to .xinitrc"
 ln -s $PWD/xresources.d/xinitrc $HOME/.xinitrc
 
-echo "\t---> Merge 'xrdb'"
+ech "\t---> Merge 'xrdb'"
 xrdb -merge -I$HOME ~/.Xresources
 echo "===> Done bootstrap .Xresources"
 # End X configs
@@ -42,8 +42,8 @@ ln -s $PWD/zhs/zshrc $HOME/.zshrc
 # Install Antigen ZSH plugin manager
 echo "===> Installing zsh plugin manager Antigen"
 echo "---> Clone from oficial repo to $HOME/antigen"
-git clone https://github.com/zsh-users/antigen.git $HOME/antigen
-
+# git clone https://github.com/zsh-users/antigen.git $HOME/.antigen
+yaourt -S antigen-git
 echo "You have several alternative methods to install Antigen as well.
 
 Using Debian package:
@@ -62,8 +62,8 @@ echo "---> Install zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
 # Install python packages
 
-echo "---> Install Python development tools"
-sudo apt-get install -y python3-dev python3-pip
+echo "---> [FIXME}: Install Python development tools"
+# sudo apt-get install -y python3-dev python3-pip
 
 echo "---> Install Pyhton PIP packages"
 pip3 install --upgrade pip
@@ -84,8 +84,9 @@ echo "\t Done!\n"
 
 # Install vim
 
-echo "---> Install vim editor"
-sudo apt-get install -y ncurses-term exuberant-ctags vim vim-gnome python-dev tmux
+echo "---> [FIXME] Install vim editor"
+# sudo apt-get install -y ncurses-term exuberant-ctags vim vim-gnome python-dev tmux
+sudo pacman -Sy vim vim-gnome ctags
 echo "\t Done!\n"
 
 echo "---> Config vim"
@@ -93,14 +94,14 @@ ln -s $PWD/vim $HOME/.vim
 echo "\t Done!\n"
 
 
-echo "NUMIX THEME"
+echo "[FIXME] NUMIX THEME"
 echo "---> Adding numix ppa to source list ..."
-sudo add-apt-repository ppa:numix/ppa
-sudo apt-get update
+# sudo add-apt-repository ppa:numix/ppa
+# sudo apt-get update
 echo "\t[OK]"
 
-echo "---> Installing numix..."
-sudo apt-get install -y numix-gtk-theme numix-icon-theme-circle numix-* unity-tweak-tool
+echo "---> [FIXME] Installing numix..."
+# sudo apt-get install -y numix-gtk-theme numix-icon-theme-circle numix-* unity-tweak-tool
 echo "\t [OK]"
 echo "\t [Done]"
 

9eb1f0e42368eb80207aed7e93fedd6dae52a41a
Paulo Patto <paulopatto@paulopatto.com>
2019-01-11 16:40:12 -0200

    Add asdf configs and vim alias

---
+++ asdf-vm/asdf.zsh
@@ -0,0 +1,4 @@
+# Configs to ASDF-VM:
+[ -f $ASDF_HOME/asdf.zsh ] && source $ASDF_HOME/asdf.sh
+[ -f $ASDF_HOME/completons/asdf.bash ] && source $ASDF_HOME/completions/asdf.bash
+# End of ASDF-VM
--- asdf-vm/install.sh
+++ asdf-vm/install.sh
@@ -8,13 +8,7 @@ git clone https://github.com/asdf-vm/asdf.git $ASDF_HOME
 
 if [ -d "$ASDF_HOME"  ]; then
   echo "--> Add refs. to .zhsrc"
-  echo "
-# Configs to ASDF-VM:
-[ -f $ASDF_HOME/asdf.zsh ] && source $ASDF_HOME/asdf.sh
-[ -f $ASDF_HOME/completons/asdf.bash ] && source $ASDF_HOME/completions/asdf.bash
-# End of ASDF-VM
-  " >> $HOME/.zshrc
-
+  ln -s $HOME/Code/asdf/asdf.zsh $HOME/.asdf.zsh
 
   echo "--> Install asdf-vm plugins: "
 

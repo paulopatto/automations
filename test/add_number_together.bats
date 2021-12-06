0304ef620baf3ba70768e896dc06a5de31a72533
Paulo Patto <paulopatto@paulopatto.com>
2021-12-06 18:55:37 -0300

    feature: demo gh actions (#61)
    Add support to github action and bats.

--- test/add_number_together.bats
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bats
-# vim: set ft=sh :
-
-load 'bats_helper'
-
-@test "should add numbers together" {
-  assert_equal $(echo 1+1 | bc) 2
-}
--- test/bats_helper.bash
+++
@@ -1,7 +0,0 @@
-#!.test/support/bats/bin/bats sh
-# vim: set ft=sh :
-
-load 'support/bats-support/load'
-load 'support/bats-assert/load'
-load 'support/bats-file/load'
-

05a45441dc5b1d9eb437b01c907d2902c16b5c0c
Paulo Patto <paulopatto@paulopatto.com>
2022-04-25 23:51:14 -0300

    Remove ansible mode:
    + Add deafult packages to Ruby and NodeJS on ASDF
    + Install ASDF via shell script
    ~ Update neovim to use script instead of ansible (WIP)
    + Using TPM plugin manager to tmux

---
+++ tmux/install.sh
@@ -0,0 +1,6 @@
+
+mkdir -p $HOME/.tmux/themes
+mkdir -p $HOME/.tmux/plugins
+
+git clone --depth 1 https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+git clone --depth 1 https://github.com/erikw/tmux-powerline.git $HOME/.tmux-powerline
